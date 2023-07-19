9fdb7265774e5084fe227121545c136bc28d76f8
carolinekle <caroline.klewinowski@gmail.com>
2023-07-18 22:29:35 -0400

    get started on js. query select and start constructor

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const addButton = document.querySelector(".add")
+const addBook = document.querySelector(".add")
 const stack = document.querySelector(".stack")
 const exitModal =document.querySelector(".exitModal")
 const submitBook = document.querySelector(".submit")
+const removeBook = document.querySelector(".remove-book")
+const bookTitle = document.querySelector('[name="title"]');
+const bookAuthor = document.querySelector('[name="author"]');
+const bookPages = document.querySelector("[data-pages]");
+const readOrNot = document.querySelector("[data-red-or-not]");
 
 class book{
     constructor(author, title, page, read){
@@ -9,11 +14,23 @@ class book{
         this.title = title;
         this.page = page;
         this.read = read;
-        this.clear();
+        this.addBook();
     }
+
+    addBook(){
+        c
+    }    
 }
 const book = new book(author, title, page, read)
 
-addButton.addEventListener(click, () => {
+addBook.addEventListener(click, () => {
+    book.addBook()
+})
 
+exitModal.addEventListener("click", () =>{
+    /* refresh modal? */
+})
+
+removeBook.addEventListener("click", () =>{
+    book.removeBook()
 })
